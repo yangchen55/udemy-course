2e3ffbd6f348c1ab25c7369681c2c31201937e14
yangchen55 <tseyang986@gmail.com>
2022-12-01 20:31:28 +1100

    initial messgae

---
+++ server.js
@@ -0,0 +1,25 @@
+//jshint eversion:6
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html")
+   
+
+})
+app.post("/", function(req, res){
+    var num1 =+ req.body.num1;
+    var num2 =+ req.body.num2;
+    var result = num1 + num2;
+
+res.send("thanks for posting" + result)
+}
+)
+
+
+app.listen(3000, function(){
+    console.log("it is good");
+})
